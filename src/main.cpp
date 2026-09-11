8be0d881ce344782f2e3e4c0ee3beef8a9059b11
Ritesh DSB <1briteshs@gmail.com>
2026-09-12 03:00:34 +0530

    Refactor gyro and background task implementations

--- src/main.cpp
+++ src/main.cpp
@@ -34,71 +34,130 @@ Espfc::Espfc espfc;
     #include <freertos/task.h>
     #include <driver/timer.h>
 
-    TaskHandle_t gyroTaskHandle = NULL;
-    TaskHandle_t pidTaskHandle = NULL;
-    static const timer_group_t TIMER_GROUP = TIMER_GROUP_0;
-    static const timer_idx_t TIMER_IDX = TIMER_0;
+  TaskHandle_t gyroTaskHandle = NULL;
+TaskHandle_t backgroundTaskHandle = NULL;
 
-    bool IRAM_ATTR gyroTimerIsr(void* args)
+static const timer_group_t TIMER_GROUP = TIMER_GROUP_0;
+static const timer_idx_t TIMER_IDX = TIMER_0;
+
+bool IRAM_ATTR gyroTimerIsr(void* args)
+{
+  BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+  vTaskNotifyGiveFromISR(
+      gyroTaskHandle,
+      &xHigherPriorityTaskWoken);
+
+  return xHigherPriorityTaskWoken == pdTRUE;
+}
+
+void gyroTimerInit(bool (*isrCb)(void* args), int interval)
+{
+  timer_config_t config = {
+    .alarm_en = TIMER_ALARM_EN,
+    .counter_en = TIMER_PAUSE,
+    .intr_type = TIMER_INTR_LEVEL,
+    .counter_dir = TIMER_COUNT_UP,
+    .auto_reload = TIMER_AUTORELOAD_EN,
+    .divider = 80,
+  };
+
+  timer_init(
+      TIMER_GROUP,
+      TIMER_IDX,
+      &config);
+
+  timer_set_counter_value(
+      TIMER_GROUP,
+      TIMER_IDX,
+      0);
+
+  timer_set_alarm_value(
+      TIMER_GROUP,
+      TIMER_IDX,
+      interval);
+
+  timer_isr_callback_add(
+      TIMER_GROUP,
+      TIMER_IDX,
+      isrCb,
+      nullptr,
+      ESP_INTR_FLAG_IRAM);
+
+  timer_enable_intr(
+      TIMER_GROUP,
+      TIMER_IDX);
+
+  timer_start(
+      TIMER_GROUP,
+      TIMER_IDX);
+}
+
+void gyroTask(void* pvParameters)
+{
+  (void)pvParameters;
+
+  espfc.begin();
+
+  gyroTimerInit(
+      gyroTimerIsr,
+      espfc.getGyroInterval());
+
+  while (true)
+  {
+    ulTaskNotifyTake(
+        pdTRUE,
+        portMAX_DELAY);
+
+    espfc.update(true);
+  }
+}
+
+void backgroundTask(void* pvParameters)
+{
+  (void)pvParameters;
+
+  while (true)
+  {
+    while (espfc.updateOther())
     {
-     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-      vTaskNotifyGiveFromISR(gyroTaskHandle, &xHigherPriorityTaskWoken);
-      return xHigherPriorityTaskWoken == pdTRUE;
+      // Drain non-flight-critical queued events.
     }
 
-    void gyroTimerInit(bool (*isrCb)(void* args), int interval)
-    {
-      timer_config_t config = {
-        .alarm_en = TIMER_ALARM_EN,
-        .counter_en = TIMER_PAUSE,
-        .intr_type = TIMER_INTR_LEVEL,
-        .counter_dir = TIMER_COUNT_UP,
-        .auto_reload = TIMER_AUTORELOAD_EN,
-        .divider = 80,
-      };
-      timer_init(TIMER_GROUP, TIMER_IDX, &config);
-      timer_set_counter_value(TIMER_GROUP, TIMER_IDX, 0);
-      timer_set_alarm_value(TIMER_GROUP, TIMER_IDX, interval);
-      timer_isr_callback_add(TIMER_GROUP, TIMER_IDX, isrCb, nullptr, ESP_INTR_FLAG_IRAM);
-      timer_enable_intr(TIMER_GROUP, TIMER_IDX);
-      timer_start(TIMER_GROUP, TIMER_IDX);
-    }
+    vTaskDelay(1);
+  }
+}
 
-    void gyroTask(void *pvParameters)
-    {
-      espfc.begin();
-      gyroTimerInit(gyroTimerIsr, espfc.getGyroInterval());
-      while(true)
-      {
-        ulTaskNotifyTake(pdTRUE, portMAX_DELAY); // wait for timer isr notification
-        espfc.update(true);
-      }
-    }
+void setup()
+{
+  disableCore0WDT();
 
-    void pidTask(void *pvParameters)
-    {
-      while(true)
-      {
-        espfc.updateOther();
-      }
-    }
+  espfc.load();
 
-    void setup()
-    {
-      disableCore0WDT();
-      // internal task priorities
-      // PRO(0): hi-res timer(22), timer(1), event-loop(20), lwip(18/any), wifi(23), wpa(2/any), BT/vhci(23), NimBle(21), BT/other(19,20,22), Eth(15), Mqtt(5/any)
-      // APP(1): free
-      espfc.load();
-      xTaskCreateUniversal(gyroTask, "gyroTask", 8192, NULL, 24, &gyroTaskHandle, 1);
-      xTaskCreateUniversal(pidTask,  "pidTask",  8192, NULL,  1, &pidTaskHandle,  0);
-      vTaskDelete(NULL); // delete arduino loop task
-    }
+  xTaskCreateUniversal(
+      gyroTask,
+      "gyroTask",
+      8192,
+      NULL,
+      24,
+      &gyroTaskHandle,
+      1);
 
-    void loop()
-    {
-    }
+  xTaskCreateUniversal(
+      backgroundTask,
+      "backgroundTask",
+      4096,
+      NULL,
+      1,
+      &backgroundTaskHandle,
+      0);
 
+  vTaskDelete(NULL);
+}
+
+void loop()
+{
+}
   #elif defined(ESPFC_MULTI_CORE_RP2040)
 
     bool core1_separate_stack = true;
