b1cd7277795d6e39dce812225208d357f9e4a259
Ritesh DSB <1briteshs@gmail.com>
2026-09-12 23:57:32 +0530

    Change pin parameter type in InputPPM class

--- lib/Espfc/src/Device/InputPPM.h
+++ lib/Espfc/src/Device/InputPPM.h
@@ -16,7 +16,7 @@ namespace Device {
 class InputPPM: public InputDevice
 {
   public:
-    void begin(uint8_t pin, int mode = PPM_MODE_NORMAL);
+    void begin(int pin, int mode = PPM_MODE_NORMAL);
     InputStatus update() override;
     uint16_t get(uint8_t i) const override;
     void get(uint16_t * data, size_t len) const override;
@@ -30,11 +30,12 @@ class InputPPM: public InputDevice
     void handle();
     static void handle_isr(void* args);
 
-    volatile uint16_t _channels[CHANNELS];
-    volatile uint32_t _last_tick;
-    volatile uint8_t  _channel;
-    volatile bool     _new_data;
-    uint8_t _pin;
+volatile uint16_t _channels[CHANNELS] = {};
+volatile uint32_t _last_tick = 0;
+volatile uint8_t _channel = 0;
+volatile bool _new_data = false;
+
+int _pin = -1;
 };
 
 }
