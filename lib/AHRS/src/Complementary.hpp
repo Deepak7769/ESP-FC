6053a5773c85b3f969d62802e98f6efba891cf4c
Dna-143 <dubukwaddeepak5@gmail.com>
2026-09-20 12:25:14 +0530

    Update Complementary.hpp

--- lib/AHRS/src/Complementary.hpp
+++ lib/AHRS/src/Complementary.hpp
@@ -1,14 +1,25 @@
 #pragma once
 
-// complementary filter for fusing rate and position measurements
 class Complementary
 {
 public:
-  void begin(float sampleRate, float tau, float state = 0.0f);
-  float update(float rate, float position);
+  void begin(
+      float sampleRate,
+      float tau,
+      float state = 0.0f);
+
+  float update(
+      float rate,
+      float position);
+
+  float update(
+      float rate,
+      float position,
+      float dt);
 
 private:
   float _dt{0.001f};
+  float _tau{0.5f};
   float _alpha{0.998f};
   float _state{0.0f};
 };
